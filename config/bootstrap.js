e387ded1f9bf1b7f381bfdbe65584ae38afd87ed
Mike DeVita <m.devita@gmail.com>
2015-04-16 17:19:19 -0700

    more updates to the bootstrapping of an admin user

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -15,37 +15,40 @@ module.exports.bootstrap = function(cb) {
 
   // passport middleware to load strategies
   sails.services.passport.loadStrategies();
+  var adminUser = sails.config.adminUser;
 
-  User.findOrCreate({
-    username : 'admin',
-    email    : 'admin@azsupras.com',
-  }, function (err, user) {
-    if (err) {
-
-      return console.error('Error Seeding Admin User', err);
-
-    }else{
-      // Generating accessToken for API authentication
-      var token = crypto.randomBytes(48).toString('base64');
-      var randomPassword = crypto.randomBytes(12).toString('hex');
-
-      Passport.create({
-        protocol    : 'local',
-        password    : randomPassword,
-        user        : user.id,
-        accessToken : token
-      }, function (err, passport) {
+  User.findOne(adminUser).exec(function(err, result){
+    if(typeof result === 'undefined'){
+      console.log('Base Admin Not Found, creating it!');
+      User.create(adminUser, function (err, user) {
         if (err) {
-          console.error(err);
-          return user.destroy(function (destroyErr) {
-            console.error(err);
+          return console.error('Error Seeding Admin User', err);
+        }else{
+          // Generating accessToken for API authentication
+          var token = crypto.randomBytes(48).toString('base64');
+          var randomPassword = crypto.randomBytes(12).toString('hex');
+
+          Passport.create({
+            protocol    : 'local',
+            password    : randomPassword,
+            user        : user.id,
+            accessToken : token
+          }, function (err, passport) {
+            if (err) {
+              console.error(err);
+              return user.destroy(function (destroyErr) {
+                console.error(err);
+              });
+            }
+
+            console.log('Seeded DB With User:', user.username, '/', randomPassword);
           });
         }
-
-        console.log('Seeded DB With User:', user.username, '/', randomPassword);
       });
+
     }
   });
+
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
